895ef5fdfa467f4c47f74be393cedca54886b2d5
Admin <J_eberholst@hotmail.com>
2020-08-27 18:02:31 +0200

    ..

--- src/PersonNummer.kt
+++ src/PersonNummer.kt
@@ -1,6 +1,6 @@
 private fun main(){
 
-    printAddEntry(question = "Vänligen ange ett personnummer i format SSYYMMDDXXXX eller SSYYYYMMDDXXXX")
+    printAddEntry(question = "Vänligen ange ett personnummer i format YYMMDDXXXX eller SSYYMMDDXXXX")
     val reader = readLine()
     var pNumber = ""
     var isError = false
