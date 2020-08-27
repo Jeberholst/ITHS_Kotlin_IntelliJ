28854751e12459a2fd666df489bcb9d70c6c9b74
Admin <J_eberholst@hotmail.com>
2020-08-27 11:28:23 +0200

    ..

--- src/FizzBuzz.kt
+++ src/FizzBuzz.kt
@@ -1,20 +1,15 @@
 private fun main(){
 
     println("Welcome to FizzBuzz!")
-
     for(i in 1..100){
-        val str = returnDivision(i)
-        println(str)
+        println(returnDivision(i))
     }
-
 }
 
 private fun returnDivision(i: Int): String{
 
     val divThree =  i % 3
     val divFive=  i % 5
-
-   // println("$i $divThree $i $divFive")
 
     return when{
         (divThree == 0) and (divFive == 0) -> {"FizzBuzz"}
